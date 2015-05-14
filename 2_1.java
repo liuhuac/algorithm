a23a4b7efba1ac39906ceb322b7cfeb33ffea6b5
liuhuac <chenliuhuazju@gmail.com>
2015-05-14 09:37:09 -0400

    upload 2_1

---
+++ 2_1.java
@@ -0,0 +1,13 @@
+// CC150
+// 2.1 Write code to remove duplicates from an unsorted linked list.
+// FOLLOW UP
+// How would you solve this problem if a temporary buffer is not allowed?
+
+class Solution2_1{
+	solve(ArrayList<int> list){
+		Map m = new HashMap();
+		while(list.hasNext()){
+
+		}
+	}
+}
